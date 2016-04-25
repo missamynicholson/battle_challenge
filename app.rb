f1b9d5fa0a882d0f4e95d796fbe05657a05b564c
Amy Nicholson <missamynicholson@gmail.com>
2016-04-25 17:29:28 +0100

    Form tags

--- app.rb
+++ app.rb
@@ -20,7 +20,13 @@ get '/and_another_one' do
   "My thirde deep dark secret is..."
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name_logic = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name_logic = params[:name]
   erb(:index)
 end
